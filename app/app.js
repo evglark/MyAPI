8bf7c333a1b5004bd861f7c95bf71bb4f88f08ac
evgeniylarkin <evglark@gmail.com>
2018-11-18 22:29:00 +0300

    add cross domain request

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import logger from 'koa-logger'
+import cors from 'koa2-cors'
 
 import { IS_DEV } from '../config/env'
 import initHandlers from './handlers'
@@ -9,6 +10,8 @@ import router from './router'
 const app = new Koa();
 
 initHandlers(app);
+
+app.use(cors());
 if (IS_DEV) app.use(logger());
 app.use(bodyParser());
 app.use(router, { createIndexes: true });
